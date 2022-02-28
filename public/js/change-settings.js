18f13aa088b6a1762b67607a14b3b42635ecdaa4
saket sama <saketsama8@gmail.com>
2022-02-27 16:06:17 -0800

    cleaned up the change settings js and the css, still have to make it responsive

--- public/js/change-settings.js
+++ public/js/change-settings.js
@@ -1,188 +1,151 @@
 
 const container = document.querySelector('.change-account-settings')
-container.addEventListener('click', pickIcon);
-container.addEventListener('click', updatePassword);
 const confirmation = document.querySelector('.confirmation')
+
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+
 
 const profileImages = document.querySelector('.profile-icon-container');
 const signinForm = document.querySelector('.change-details')
+
+//gets what is being requested to change
 const getState = new httpRequest('http://localhost:5000/users/change-request')
 getState.getRequest()
 .then(settings=>{
-  console.log(settings);
+  //if user wants to change their profile image
   if(settings.profileImage){
   populateGallery();
-
-    setTimeout(showImages, 500)
+    //shows the images after half a secons
+    setTimeout(()=>{
+      profileImages.style.display = "flex";
+    }, 500)
     signinForm.style.display = "none";
   }else{
-    container.addEventListener("click", changeLogin)
+  //if user wants to change their password, defailt state
+
     const usernameLabel = document.getElementById('username-label')
     const passwordLabel = document.getElementById('password')
     const changeBtn = document.querySelector('#change-parameter')
-    if(settings.username){
-   
 
+    //if user wants to change their username
+    if(settings.username){
+
+      //changing text of labels and button
       usernameLabel.textContent = "Enter your old username";
       passwordLabel.textContent = "Enter your password";
       changeBtn.value = "Confirm account"
     }
+    //if user wants to remove their account
     if(settings.remove){
-      console.log("delete account");
       usernameLabel.textContent = "Enter your username";
       passwordLabel.textContent = "Enter your password";
       changeBtn.value = "Confirm account"
-      
     }
   }
 })
-function showImages(){
+// function showImages(){
+// }
 
-  profileImages.style.display = "flex";
-}
+//adds images to the gallery 
 async function populateGallery(){
   const gallery = document.querySelector('.gallery-container')
   const getPictures = fetch('http://localhost:5000/users/pics')
   .then(data=>data.json())
   .then(data => {
-    console.log(data)
-    
     const pics = data;
     pics.map((pic)=>{
-      const div = document.createElement('div');
-      div.classList.add('profile-icon')
       const img = document.createElement('img');
       img.classList.add('profile-option')
       img.src = pic.src;
-      div.appendChild(img);
-      gallery.appendChild(div);
+      gallery.appendChild(img);
 
     })
-
-  }
-    )
+  })
 }
 
-
+container.addEventListener('click', pickIcon);
 
 function pickIcon(e){
-
+  //gets called when a profile image is clicked
   if(e.target.classList.contains('profile-option')){
+    //dynamically added class that is added when an image is clicked
     const previousSelection = document.querySelector('.selected-profile');
-  
+    //if there is a previous selection and the user clicks that image, deselect it
     if(previousSelection && e.target.classList.contains('selected-profile')){
       previousSelection.classList.remove('selected-profile');
-      //e.target.classList.add('selected-profile')
-     
     }else{
-    
+      //if there is a previous selection deselect it and select whatever image is being clicked
         if(previousSelection){
       previousSelection.classList.remove('selected-profile');
-
         }
         e.target.classList.add('selected-profile')
-      //}
-     
-
     }
-   
   }
   
 }
-const confirmImage = document.querySelector('.confirm-new-image')
-confirmImage.addEventListener("click", sendPatchRequest)
+
 const getLoggedInUser = new httpRequest('http://localhost:5000/users');
-
-
+//function that returns the user thats logged in  
 function getUser  (){
-  let loggedInUser;
  const user =  getLoggedInUser.getRequest()
   .then(data=>{
-    let users = data;
-    loggedInUser =   users.filter((user)=>{
+    const users = data;
+    const loggedInUser =   users.filter((user)=>{
       return user.login
     })
-    if(loggedInUser.length === 1){
-      console.log(loggedInUser)
-    }
     return loggedInUser
-
   }
-  
-
   )
-  console.log(user);
-  console.log(loggedInUser);
-
   return user;
 }
-// const getLoggedInUser = new httpRequest('http://localhost:5000/users');
+
+const confirmImage = document.querySelector('.confirm-new-image')
+confirmImage.addEventListener("click", sendPatchRequest)
+
+
 function sendPatchRequest(){
   const previousSelection = document.querySelector('.selected-profile');
+  //runs if there is an image selected when button is clicked
   if(previousSelection){
-console.log('Send request')
+    //gets the logged in user
      getUser()
+     //calls the patch function with the logged in user and the url of the selected image of the new profile image
      .then(user=>{
       patch(user, previousSelection.src)
-
      });
-    
   }
-
 }
 
 
+
+
+
+//sends the request to the server to update the profile image to the one selected and redirects to the homepage
 function patch(user, url){
-  console.log(user);
   const getUserInfo = new httpRequest(`http://localhost:5000/users/${user[0].username}`)
   getUserInfo.changeProfileSettings(url);
   window.location.href = 'index.html';
 }
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('password');
 
-function changeLogin(e){
-  if(e.target.id === "change-parameter" && !e.target.classList.contains('patch-username')&& !e.target.classList.contains('delete-account')){
-    e.preventDefault()
-   
-    console.log(usernameInput.value, passwordInput.value )
-    if(usernameInput.value !=='' && passwordInput.value != ''){
-      checkIfValid(usernameInput.value, passwordInput.value);
-
-    }else{
-
-      let message = "Please Fill Out All Fields"
-    createErrorMessage(message)
-    }
-    console.log('update')
-    
-
-  }
-}
 
 const formDiv = document.querySelector('.validate-login')
 const changePassword = document.querySelector('.change-password')
 
-
+//creates the error message 
 function createErrorMessage(mesg){
   const previousMessage = document.querySelector('.alert-message');
   const usernameLabel = document.getElementById('username-label')
-
    if(previousMessage!== null) return
   const div = document.createElement('div');
   const message = document.createTextNode(mesg)
   div.classList.add('alert-message')
   div.appendChild(message);
   if(formDiv.classList.contains('hidden')){
-  console.log(formDiv.style)   
- console.log(changePassword);
-
 const passwordLabel = document.getElementById('new-password-label')
-
     changePassword.insertBefore(div, passwordLabel)
   }else{
     formDiv.insertBefore(div, usernameLabel )
-
   }
   setTimeout(removeErrorMessage, 2000, div)
  
@@ -193,36 +156,49 @@ function removeErrorMessage(div){
 }
 
 
+container.addEventListener("click", changeLogin)
 
+function changeLogin(e){
+  //the same button us used to perform all of the different requests but it is given a different class if the user wants to change their username or delete their account  this only fires if the button doesn't have the patch-username or delete-account class
+  if(e.target.id === "change-parameter" && !e.target.classList.contains('patch-username')&& !e.target.classList.contains('delete-account')){
+    e.preventDefault()
+    //if all fields are filled out check if its valid
+    if(usernameInput.value !=='' && passwordInput.value != '') checkIfValid(usernameInput.value, passwordInput.value)
+    else createErrorMessage( "Please Fill Out All Fields");
+  }
+}
+
+//checking user authentication to see if the input vlaues match the users username and password
 function checkIfValid(username, password){
  getUser()
  .then(user=>{
-   
+  //if the inputs match 
    if(user[0].username === username && user[0].password === password){
      console.log("valid")
      getState.getRequest()
      .then(update=>{
-       console.log(update)
+       //if the user wanted to change their password show the change password state
        if(update.password){
         formDiv.style.display = "none"
         formDiv.classList.add("hidden");
         changePassword.style.display = "block"
        }else{
         const changeBtn = document.getElementById("change-parameter");
-
+        passwordInput.disabled = true;
+        usernameInput.disabled = true
+        const copyBtn = changeBtn;
+        //if the user wanted to remove their account show the remove state
          if(update.remove){
-         const copyBtn = changeBtn;
-         changeBtn.remove()
-         passwordInput.disabled = true;
-         usernameInput.disabled = true;
-            confirmation.style.display = "block";
-            copyBtn.value = "Delete Account";
-
-            copyBtn.classList.add("delete-account")
-            formDiv.appendChild(copyBtn);
-         }else{
-          passwordInput.disabled = true;
-          usernameInput.disabled = true;
+          changeBtn.remove();
+          confirmation.style.display = "block";
+          copyBtn.value = "Delete Account";
+          copyBtn.classList.add("delete-account")
+          formDiv.appendChild(copyBtn);
+         }
+         //if the user wanted to change their username then show the username state
+         else{
+          // passwordInput.disabled = true;
+          // usernameInput.disabled = true;
           const label = document.createElement('label');
           label.for = "new-username"
           label.textContent = "Enter new username"
@@ -241,65 +217,63 @@ function checkIfValid(username, password){
        }
      })
    
-   }else{
-     console.log("wrong username or password")
+   }
+   else{
     createErrorMessage("wrong username or password")
-
    }
  })
 }
 
 
 const passwordLabel = document.getElementById('.new-password-label')
+container.addEventListener('click', updatePassword);
 
-function updatePassword(e){
+//changes password
+function updatePassword(e){  
   if(e.target.id === "confirm-new-password"){
     e.preventDefault();
-    console.log("running")
     const passwordInput = document.getElementById('new-password');
     const confirmPasswordInput = document.getElementById('confirm-password');
-
     if(passwordInput.value !=='' && confirmPasswordInput.value !== ''){
         if(passwordInput.value !== confirmPasswordInput.value){
-      
           createErrorMessage("Passwords don't match", passwordLabel)
-
-        }else{
+        }
+        else{
           const password = passwordInput.value
           const containsNum = /\d/;
-        const testingUsername = containsNum.test(password);
-          if(password.length <7 || !testingUsername){
-              createErrorMessage("Password Must be at least 7 character with atleast one number")
-          }else{
+          const testingPassword = containsNum.test(password);
+          if(password.length <7 ){
+              createErrorMessage("Password must be at least 7 characters");
+          }
+          else if(!testingPassword){
+            createErrorMessage("Password must have at least one number ");
+          }
+
+          //if the new password meets the criteria then send the patch request to the server with the new password
+          else{
             getUser()
             .then(user=>{
-              const setPassword = new httpRequest(`http://localhost:5000/users/${user[0].username}`)
+              const setPassword = new httpRequest(`http://localhost:5000/users/${user[0].username}`);
               setPassword.changeProfileSettings(null, null, password);
-              window.location.href = "index.html"
+              window.location.href = "index.html";
             })
-         
-
           }
-          
         }
-      ;
-
-    }else{
-      console.log("empty")
-      let message = "Please Fill Out All Fields"
-    createErrorMessage(message)
+    }
+    else{
+    createErrorMessage("Please Fill Out All Fields")
     }
   }
 
 }
 
 container.addEventListener("click", patchUsername)
-
+//changes the username
 function patchUsername(e){
   if(e.target.classList.contains("patch-username")){
     e.preventDefault();
-    console.log("update username ")
     const newUsername = document.getElementById("new-username")
+    //if theres a value inputted check if it is a valid username
     if(newUsername.value!== ''){
       checkIfUsernameValid(newUsername.value)
     }else{
@@ -312,61 +286,64 @@ function patchUsername(e){
 
 function checkIfUsernameValid(username){
   const checkNumber = /\d/;
+  //testing to see if username has a number
   const hasNumber = checkNumber.test(username);
   if(hasNumber){
+    //checking to see if username is long enough
       if(username.length <6) {
         createErrorMessage("New username must have atleast six characters")
       }else{
         getLoggedInUser.getRequest()
         .then(users=>{
+          //checking to see if there are any users that have that username already
           const filtered = users.filter((user)=>{
             return user.username === username
           })
+          //if there is a user with that username
           if(filtered.length!== 0){
-            console.log("Username is already Taken ")
             createErrorMessage("Username is already Taken ")
-          }else{
+          }
+          //send a patch request with the new username 
+          else{
           getUser()
           .then(user=>{
             const setUsername = new httpRequest(`http://localhost:5000/users/${user[0].username}`)
             setUsername.changeProfileSettings(null, username, null);
             window.location.href = "index.html"
           })
-        
-            
           }
         })
       }
+
   }else[
     createErrorMessage("New username must have atleast one number")
   ]
 }
 
 container.addEventListener("click", removeAccount)
+//removes account
 function removeAccount(e){
   if(e.target.classList.contains('delete-account')){
     e.preventDefault();
     const confirmDelete = document.getElementById("confirm-delete");
     const dontDelete = document.getElementById("dont-delete");
-    console.log(confirmDelete.checked, dontDelete.checked)
+    //if none of the options are checked 
     if(!confirmDelete.checked && !dontDelete.checked){
       createErrorMessage("Please check yes or no")
     }else{
+      //if user decides not to delete
       if(dontDelete.checked){
         window.location.href = "index.html"
       }else{
+        //send delete request to the server to remove the account
         getUser()
         .then(user=>{
           const deleteAccount = new httpRequest(`http://localhost:5000/users/${user[0].username}`)
           deleteAccount.deleteAccount();
-          //setUsername.changeProfileSettings(null, username, null);
 
           window.location.href = "index.html"
         })
-        
-
       }
     }
-    console.log("Remove Account");
   }
 }
