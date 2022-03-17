d70e71e3abeb7ae38ee76b7fde5a2a2824a84c1d
Saket Sama <saketsama8@gmail.com>
2022-03-17 00:47:25 -0700

    updated url

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
 import usersRoutes from "./routes/users.js";
+import cors from "cors";
+
 const app = express();
-
 const port = process.env.PORT || 5000;
-
+app.use(
+	cors({
+		origin: "*",
+	})
+);
 app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use("/users", usersRoutes);
@@ -14,5 +19,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-	console.log(`Server running on port: http://${port}`);
+	console.log(`Server running on port: http://localhost:${port}/`);
 });
